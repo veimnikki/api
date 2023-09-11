bedf3d35ff83a189e890e0e128b9a7cef5939359
nikka <veimnikki@gmail.com>
2023-09-11 11:28:53 +0200

    get_coin_fee method and requestLimit were added

--- wooX.py
+++ wooX.py
@@ -1,6 +1,7 @@
 import requests
 import aiohttp
 import asyncio
+import time
 
 class Woo:
     def __init__(self):
@@ -8,6 +9,8 @@ class Woo:
         self.urlOrderbooks = f"https://api.woo.org/v1/public/orderbook/"
         self.urlMarkets = f"https://api.woo.org/v1/public/info"
         self.markets = {}
+        self.fees = {"Maker": 0.05, "Taker": 0.05}
+        self.requestLimit = 5  # ban on iteration 6
 
     def get_markets(self):
         markets = requests.get(url=self.urlMarkets, headers=self.headers).json()
@@ -18,21 +21,30 @@ class Woo:
         return (self.markets)
         # return(markets)
 
+    def get_coin_fee(self, symbol):
+        return {symbol: self.fees}
+
     async def get_orderbook(self, symbol):
         async with aiohttp.ClientSession() as session:
             async with session.get(self.urlOrderbooks + symbol) as response:
                 ob = await response.json()
                 # print(ob)
-        return {"top_ask": ob['asks'][0]['price'], "top_bid": ob['bids'][0]['price']}
-
-woo = Woo()
-markets = woo.get_markets()
-print(markets)
+        return {"top_ask": ob['asks'][0]['price'], "ask_vol": ob['asks'][0]['quantity'],
+                "top_bid": ob['bids'][0]['price'], "bid_vol": ob['bids'][0]['quantity'],
+                "ts_exchange": ob['timestamp']}
 
 async def main():
     orderbook = Woo()
-    result = await orderbook.get_orderbook('SPOT_BTC_USDT')
-    print(result)
+    # result = await orderbook.get_orderbook('SPOT_BTC_USDT')
+    # print(result)
+    while True:
+        t0 = time.time()
+        result = asyncio.create_task(orderbook.get_orderbook('SPOT_BTC_USDT'))
+        await result
+        print(time.time() - t0)
 
 if __name__ == "__main__":
+    markets = Woo()
+    print(markets.get_markets())
+    print(markets.get_coin_fee('BTC_USDT'))
     asyncio.run(main())
